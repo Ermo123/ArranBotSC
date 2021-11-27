5a3ef1b7b41b905fa45764ae8a3e83c3ab47d26a
Leon <44817515+Ermo123@users.noreply.github.com>
2021-11-26 18:58:11 -0600

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,18 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':stop_sign:',
+        queue: ':clipboard:',
+        music: ':musical_score:',
+        success: ':ballot_box_with_check:',
+        natbutt: ':two_hearts:',
+    },
+
+    discord: {
+        token: 'ODg4NjA0NDQxNjk0MDY0NjQx.YUVHYA.7260XU1dZao24C3BpyblhJ4G77w',
+        prefix: '%',
+        activity: 'Listening to SoundCloud 🟠',
+    },
+
+    
+};
